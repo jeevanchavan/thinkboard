a73b59ecd6c2805a6ce1b1d65adab828d5448357
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-24 14:28:55 +0530

    done with backend server, at last implemented rate limiter and custom middleware for rate limiting and production ready code practices

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from '../models/Note.js'
 
 export const getAllNotes = async (req,res)=>{
     try {
-        const notes = await Note.find().sort({createdAt:-1});
+        const notes = await Note.find().sort({createdAt:-1});  // latest note appear first
         res.status(200).json(notes)
     } catch (error) {
         console.error('Error in getAllNotes Controller',error)
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,23 @@ import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// middleware to access req.body
 app.use(express.json())
+app.use(rateLimiter)
 
 app.use("/api/notes",notesRoutes);
 
-connectDB()
-
-app.listen(PORT, ()=>{
-    console.log("server is listening at port",PORT);
+connectDB().then(() => {
+    app.listen(PORT, ()=>{
+        console.log("server is listening at port",PORT);
+    })
 })
+
+
