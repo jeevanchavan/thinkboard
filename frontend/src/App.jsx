fde14ac56e115f027f6ee0e1ef6739778575a1aa
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-01-25 13:37:42 +0530

    Home page ui done, implemented use states, Notecard ui, date formating in lib file, props passing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,11 @@ import NoteDetailPage from './pages/NoteDetailPage';
 const App = () => {
   return (
     <div data-theme="forest">
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/create' element={<CreatePage />} />
-        <Route path='/note/:id' element={<NoteDetailPage />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/create' element={<CreatePage />} />
+          <Route path='/note/:id' element={<NoteDetailPage />} />
+        </Routes>
     </div>
   )
 }
